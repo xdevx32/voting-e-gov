e92cb1da5205f834705f4674c839565cc179331f
xdevx32 <angelkukushev333@gmail.com>
2019-10-03 17:02:52 +0300

    Initial committ

---
+++ src/test/java/com/example/egov/voting/prototype/demo/egov/voting/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egov.voting.prototype.demo.egov.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
