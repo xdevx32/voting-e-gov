e430ac40a65b25bbca44b12dd4b9ab557e0c646b
xdevx32 <angelkukushev333@gmail.com>
2019-10-10 16:31:27 +0300

    Renames a class and adds main properties

---
+++ src/main/java/com/example/egov/voting/prototype/demo/egov/voting/people/Candidate.java
@@ -0,0 +1,15 @@
+package com.example.egov.voting.prototype.demo.egov.voting.people;
+
+public class Candidate {
+
+    public final String name;
+    public final String party;
+    public final String number;
+
+
+    public Candidate(String name, String party, String number) {
+        this.name = name;
+        this.party = party;
+        this.number = number;
+    }
+}
--- src/main/java/com/example/egov/voting/prototype/demo/egov/voting/people/Voter.java
+++
@@ -1,6 +0,0 @@
-package com.example.egov.voting.prototype.demo.egov.voting.people;
-
-public class Voter {
-    private String name;
-
-}
