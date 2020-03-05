3b008a02db037a80e5ac5b4cd328e06ad6c2163c
xdevx32 <angelkukushev333@gmail.com>
2020-03-05 19:19:00 +0200

    adds a basic use case

--- src/main/java/com/example/egov/voting/prototype/demo/egov/voting/WebAppController.java
+++ src/main/java/com/example/egov/voting/prototype/demo/egov/voting/WebAppController.java
@@ -5,11 +5,16 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 @Controller
 public class WebAppController {
     private String appMode;
+
+    @Autowired
+    private PartyRepository partyRepository;
 
     @Autowired
     public WebAppController(Environment environment){
@@ -32,5 +37,27 @@ public class WebAppController {
         model.addAttribute("mode", appMode);
 
         return "admin";
+    }
+
+    @RequestMapping("/vote")
+    public String vote(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Acho");
+        model.addAttribute("mode", appMode);
+
+        //model.addAttribute("parties" , partyRepository.findAll());
+        ArrayList<Party> parties = new ArrayList<>();
+        parties.add(new Party("Алтернативна социаллиберална партия", "4"));
+        parties.add(new Party("Българска социалдемократическа партия", "3"));
+        parties.add(new Party("Българска средна класа", "6"));
+        parties.add(new Party("БЗНС – Народен съюз", "19"));
+        parties.add(new Party("ГЕРБ", "44"));
+        parties.add(new Party("Движение „Да, България!“", "22"));
+        parties.add(new Party("Национално движение за права и свободи", "12"));
+        parties.add(new Party("Национален идеал за единство", "23"));
+        parties.add(new Party("Нова зора", "54"));
+        model.addAttribute("parties" , parties);
+
+        return "voting-page";
     }
 }
