3bf23620d519dd3bbc774648626c89145202b259
xdevx32 <angelkukushev333@gmail.com>
2020-05-30 01:39:50 +0300

    Fix Citizen. Whew.

--- src/main/java/com/nbu/evote/repository/CitizenRepository.java
+++ src/main/java/com/nbu/evote/repository/CitizenRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CitizenRepository extends CrudRepository<Citizen, Long>{
 
-    @Query(value = "SELECT * FROM Citizen a WHERE unique_vote_id = ?1 AND egn = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM citizen a WHERE unique_vote_id = ?1 AND egn = ?2", nativeQuery = true)
     Citizen findByEGNAndUniqueVoteId(String uniqueVoteId, String EGN);
 }
